35f25ccf7779c8e14390bb1ba6336b65759dcd76
Eduardo Junior Pereira Garcia <eduardo.ab.pereira@outlook.com>
2023-12-09 12:05:35 -0300

    added page function in IBeneficiarioRepository

--- src/main/java/ejpg/ekan/poc/data/repositories/IBeneficiarioRepository.java
+++ src/main/java/ejpg/ekan/poc/data/repositories/IBeneficiarioRepository.java
@@ -1,5 +1,7 @@
 package ejpg.ekan.poc.data.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import ejpg.ekan.poc.data.domain.Beneficiario;
 
 @Repository
 public interface IBeneficiarioRepository extends CrudRepository<Beneficiario, String> {
+
+    Page<Beneficiario> findAll(Pageable page);
 
 }
